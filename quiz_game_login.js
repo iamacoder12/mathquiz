e306d2d5e3d9935e053d7b454c171f9a0b1ce283
iamacoder12 <109200031+iamacoder12@users.noreply.github.com>
2023-05-12 18:27:06 +1000

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function adduser(){
+player1_name = document.getElementById("username1").value;
+player2_name = document.getElementById("username2").value;
+
+localStorage.setItem("player1_name", player1_name);
+localStorage.setItem("player2_name", player2_name);
+
+window.location = "game_page.html";
+}
+
